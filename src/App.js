159f23bb8567a90d7e6cb7c2400bdeb965846ef0
mikolvj <mikolajborowskiii@gmail.com>
2022-10-30 23:54:17 +0100

    first mock frontend done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import classes from './App.module.scss';
+import { useState } from 'react';
+
+const initResult = 0;
+const buttons = [
+	'C',
+	'^',
+	'*',
+	'/',
+	7,
+	8,
+	9,
+	'-',
+	4,
+	5,
+	6,
+	'+',
+	'1',
+	'2',
+	'3',
+	'=',
+	'0',
+	'00',
+	'.',
+];
 
 const App = () => {
+	const { value, setValue } = useState({
+		value: 0,
+		sign: '',
+		result: 0,
+	});
+
 	return (
-        <div className={classes.wrapper}>
-            <h1>elo</h1>
-        </div>
-    );
+		<div className={classes.wrapper}>
+			<div className={classes.display}>{value ? value : initResult}</div>
+			<div className={classes.buttons}>
+				{buttons.map((btn) => (
+					<div className={btn === '=' ? classes.equals : null}>{btn}</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default App;
