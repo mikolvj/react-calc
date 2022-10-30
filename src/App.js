99a3490cf419873852b1ec6aba17a9e8513f4586
mikolvj <mikolajborowskiii@gmail.com>
2022-10-31 00:32:55 +0100

    handling of dot and nums done

--- src/App.js
+++ src/App.js
@@ -25,18 +25,51 @@ const buttons = [
 ];
 
 const App = () => {
-	const { value, setValue } = useState({
-		value: 0,
-		sign: '',
-		result: 0,
-	});
+	const [value, setValue] = useState('');
 
+	const handleClear = () => {
+		setValue('');
+	};
+
+	const handleEquals = () => {
+		console.log('equals');
+	};
+	const handleSign = (e) => {
+		console.log(e.target.innerHTML);
+	};
+	const handleNum = (e) => {
+		console.log(e.target.innerHTML);
+		console.log(value);
+		if (value.length < 9) {
+			if (e.target.innerHTML === '.' && value === '') setValue('0.');
+			else if (e.target.innerHTML === '.' && value.includes('.')) return;
+			else setValue(value + e.target.innerHTML);
+		} else return;
+	};
 	return (
 		<div className={classes.wrapper}>
 			<div className={classes.display}>{value ? value : initResult}</div>
 			<div className={classes.buttons}>
-				{buttons.map((btn) => (
-					<div className={btn === '=' ? classes.equals : null}>{btn}</div>
+				{buttons.map((btn, i) => (
+					<div
+						key={i}
+						className={btn === '=' ? classes.equals : null}
+						value={btn}
+						onClick={
+							btn === '='
+								? handleEquals
+								: btn === '+' ||
+								  btn === '-' ||
+								  (btn === '/') | (btn === '*') ||
+								  btn === '^'
+								? handleSign
+								: btn === 'C'
+								? handleClear
+								: handleNum
+						}
+					>
+						{btn}
+					</div>
 				))}
 			</div>
 		</div>
