4604cd463b893929b7006ff33389059d1040a91b
mikolvj <mikolajborowskiii@gmail.com>
2022-11-01 20:49:02 +0100

    working without keyboard

--- src/App.js
+++ src/App.js
@@ -23,19 +23,127 @@ const buttons = [
 	'.',
 ];
 
+const ACTIONS = {
+	ADD_DIGIT: 'adding-digit',
+	SET_SIGN: 'setting-operation-sigh',
+	CLEAR: 'clearing-screen',
+	EVALUATE: 'evaluate',
+};
+
+const reducer = (state, { type, payload }) => {
+	switch (type) {
+		case ACTIONS.ADD_DIGIT:
+			if (state.overwrite)
+				return { ...state, currNum: payload, overwrite: false };
+
+			if (payload === '00' && state.currNum === '')
+				return { ...state, currNum: '0' };
+			if (payload === '0' && state.currNum === '0') return state;
+			if (payload === '00' && state.currNum === '0') return state;
+			if (payload === '.' && state.currNum.includes('.')) return state;
+			if (payload === '.' && state.currNum === '')
+				return { ...state, currNum: '0.' };
+
+			return {
+				...state,
+				currNum: `${state.currNum || ''}${payload}`,
+			};
+
+		case ACTIONS.SET_SIGN:
+			if (state.currNum == null && state.prevNum == null) return state;
+			if (state.currNum == null)
+				return {
+					...state,
+					sign: payload,
+				};
+			if (state.prevNum == null)
+				return {
+					...state,
+					sign: payload,
+					prevNum: state.currNum,
+					currNum: null,
+				};
+			return {
+				...state,
+				prevNum: evaluate(state),
+				currNum: null,
+				sign: payload,
+			};
+
+		case ACTIONS.CLEAR:
+			return {};
+
+		case ACTIONS.EVALUATE:
+			if (state.prevNum == null || state.currNum == null || state.sign == null)
+				return state;
+			return {
+				...state,
+				currNum: evaluate(state),
+				prevNum: null,
+				sign: null,
+
+				overwrite: true,
+			};
+	}
+};
+
+const evaluate = ({ prevNum, currNum, sign }) => {
+	const prev = parseFloat(prevNum);
+	const curr = parseFloat(currNum);
+	if (isNaN(prev) || isNaN(curr)) return '';
+	let result = '';
+	switch (sign) {
+		case '+':
+			result = prev + curr;
+			break;
+		case '-':
+			result = prev - curr;
+			break;
+		case '/':
+			if (curr == 0) result = 'ERR';
+			else result = prev / curr;
+			break;
+		case '*':
+			result = prev * curr;
+			break;
+		case '^':
+			result = Math.pow(prev, curr);
+			break;
+	}
+	return result.toString();
+};
+
 const App = () => {
+	const [{ prevNum, currNum, sign }, dispatch] = useReducer(reducer, {});
+
 	return (
 		<div className={classes.wrapper}>
 			<div className={classes.display}>
-				<div className={classes.previousNumber}>123</div>
-				<div className={classes.sign}>+</div>
-				<div className={classes.currentNumber}>456</div>
+				<div className={classes.previousNumber}>{prevNum}</div>
+				<div className={classes.sign}>{sign}</div>
+				<div className={classes.currentNumber}>{currNum}</div>
 			</div>
 			<div className={classes.buttons}>
 				{buttons.map((btn, i) => (
-					<div key={i} className={btn === '=' ? classes.equals : null}>
+					<button
+						key={i}
+						className={btn === '=' ? classes.equals : null}
+						onClick={
+							btn === '='
+								? () => dispatch({ type: ACTIONS.EVALUATE })
+								: btn === 'C'
+								? () => dispatch({ type: ACTIONS.CLEAR })
+								: btn === '+' ||
+								  btn === '-' ||
+								  btn === '/' ||
+								  btn === '*' ||
+								  btn === '^'
+								? () => dispatch({ type: ACTIONS.SET_SIGN, payload: btn })
+								: () => dispatch({ type: ACTIONS.ADD_DIGIT, payload: btn })
+						}
+					>
 						{btn}
-					</div>
+					</button>
 				))}
 			</div>
 		</div>
